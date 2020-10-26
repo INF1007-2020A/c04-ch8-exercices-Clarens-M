6679f0f99846ec10d4c423e50b84105dfa9e5041
Clarens-M <clarens.mpouo@polymtl.ca>
2020-10-26 23:20:25 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -12,7 +12,7 @@ def comparateur(fichier1, fichier2):
         for index, line in enumerate(f1):
             ligne = f2.readline()
                 if line != ligne:
-                    print("Les fichiers sont pas exactement les mêmes à la ligne {index}")
+                    print(f"Les fichiers sont pas exactement les mêmes à la ligne {index}")
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
