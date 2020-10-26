d11a4fba857722c6ffe5844019a589abe8edfce8
Clarens-M <70984346+Clarens-M@users.noreply.github.com>
2020-10-26 19:16:06 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -4,10 +4,19 @@
 PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80, 85], "C+": [75, 80], "C": [70, 75], "F": [0, 70]}
 
 # TODO: Importez vos modules ici
-
+import recettes
 
 # TODO: Définissez vos fonction ici
-
+def comparateur(fichier1, fichier2):
+    test1 = []
+    test2 = []
+    with open("fichier1", "r") as f1, open("fichier2", "r") as f2:
+        for line in f1:
+            for ligne in ff2:
+                test1 = line
+                test2 = ligne
+                if test1 != test2:
+                    return None
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
