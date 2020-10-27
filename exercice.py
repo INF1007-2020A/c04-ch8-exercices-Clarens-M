6d73fa76a015d8dfa7d393811e51054309cc7f9d
Clarens-M <clarens.mpouo@polymtl.ca>
2020-10-27 23:30:39 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -51,7 +51,7 @@ def exercice4(file_path="./ livre.p"):
         elif demande == "M":
             recette = recettes.add_recipes(recette)
         elif demande == "S":
-            
+            nom = str(input("Laquelle? \n "))
         else:
             print("Mauvais choix")
 
