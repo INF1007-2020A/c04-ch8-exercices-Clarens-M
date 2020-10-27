890fff1912cc75bae5316d7b86903eb898788848
Clarens-M <clarens.mpouo@polymtl.ca>
2020-10-27 22:47:38 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -39,9 +39,9 @@ def mention(notes):
 def exercice4():
     demande = str(input("Souhaitez-vous ajouter, modifier ou supprimer des recettes? \n"))
     recettes.print_recipe()
-    if demande == ""
-    with open("livre_de_recettes.txt", "w") as l:
-        
+    if demande == "ajouter":
+        with open("livre_de_recettes.txt", "w") as l:
+            
 
 
 if __name__ == '__main__':
