40f66e92785f65dca213dc967250c5f1b52c57d4
Clarens-M <clarens.mpouo@polymtl.ca>
2020-10-27 23:11:55 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -39,9 +39,9 @@ def mention(notes):
 
 def exercice4(file_path="./ livre.p"):
     if path.exists(file_path):
-
-    else:
         
+    else:
+
     
     demande = str(input("Souhaitez-vous ajouter, modifier ou supprimer des recettes? \n"))
     if demande == "quitter":
