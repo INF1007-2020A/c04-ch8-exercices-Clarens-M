1b59dc1a011e847ab355312ceb4a8d2a0a4fa2f8
Clarens-M <clarens.mpouo@polymtl.ca>
2020-10-27 22:18:13 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -31,7 +31,7 @@ def mention(notes):
 
         for elem in liste:
             for key, value in PERCENTAGE_TO_LETTER.items():
-                if value[0] <= elem
+                if value[0] <= int(elem) and int()
 
     pass            
 
