1651097062c1a97f451c0b4208ff2a7f3ad7c1c5
Clarens-M <clarens.mpouo@polymtl.ca>
2020-10-27 22:55:24 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -52,7 +52,7 @@ def exercice4():
                         if nouv == "Le nom":
                             line.replace(recherche, nouv)
                         elif nouv == "Les ingrédients":
-                            line.write("{" + recherche + ":" + par + "}")
+                            line.write("{" + recherche + ": " + par + "}")
 
 
 if __name__ == '__main__':
