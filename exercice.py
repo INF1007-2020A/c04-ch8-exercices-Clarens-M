36f6e19951fa6b3858bd43e53907974e03d2acf7
Clarens-M <clarens.mpouo@polymtl.ca>
2020-10-27 22:47:03 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -37,7 +37,7 @@ def mention(notes):
 
 
 def exercice4():
-    demande = str(input("Souhaitez-vous ajouter, modifier ou supprimer des recettes? "))
+    demande = str(input("Souhaitez-vous ajouter, modifier ou supprimer des recettes? \n"))
     recettes.print_recipe()
     with open("livre_de_recettes.txt", "w") as l:
         
