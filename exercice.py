a63f0fa33236ffd48be5cc9f86b09fc347155b17
Clarens-M <clarens.mpouo@polymtl.ca>
2020-10-27 23:10:00 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -40,7 +40,8 @@ def mention(notes):
 def exercice4(file_path="./ livre.p"):
     demande = str(input("Souhaitez-vous ajouter, modifier ou supprimer des recettes? \n"))
     if demande == "quitter":
-        print("")
+        print("Bon choix")
+        
     with open("livre_de_recettes.txt", "r") as L:
         lignes = L.read().splitlines()
 #    with open("livre_de_recettes.txt", "w") as l:    
